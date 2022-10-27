e9b75a9d51ad72413fb60d1fdfe6130d05742a03
Darshan Prajapati <darshanprajapati@Darshans-MacBook-Air.local>
2022-10-27 16:02:24 +0530

    http changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
     <title>Avidus Interactive</title>
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/favicon-avidus.svg')}}">
